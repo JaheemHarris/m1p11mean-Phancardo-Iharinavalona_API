031cb3085f7e9a517d67126eb32dbd99dd6117eb
Iharinavalona Rakotoniaina <iharinavalona.rakotoniaina@versusmind.eu>
2024-02-17 07:56:38 +0100

    initialize project: create project structure

---
+++ models/service.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const serviceSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	duration: {
+		type: Number,
+		required: true,
+	},
+	commission: {
+		type: Number,
+		required: true,
+	},
+	image: {
+		type: String,
+		required: false,
+	},
+	endDate: {
+		type: Date,
+		required: true,
+	},
+	isActivated: {
+		type: Boolean,
+		default: true,
+	},
+});
+
+// ADD created_at and update_at fields
+serviceSchema.set("timestamps", true);
+const Servicedb = mongoose.model("service", serviceSchema);
+module.exports = Servicedb;
