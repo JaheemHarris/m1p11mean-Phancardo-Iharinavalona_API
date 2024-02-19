6b21b93477512e1078110b0aaa0a04bd0beef954
Iharinavalona Rakotoniaina <iharinavalona.rakotoniaina@versusmind.eu>
2024-02-19 20:03:09 +0100

    add request body validator example

---
+++ middlewares/customer.middleware.js
@@ -0,0 +1,19 @@
+const { body, validationResult } = require("express-validator");
+
+const validateUser = [
+	body("firsname").notEmpty().withMessage("Firstname is required"),
+	body("lastname").notEmpty().withMessage("Lastname is required"),
+	body("email").isEmail().withMessage("Invalid email"),
+	body("password").notEmpty().withMessage("Password is required"),
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		next();
+	},
+];
+
+module.exports = {
+	validateUser,
+};
